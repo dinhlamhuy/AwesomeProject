b787486857644116cdd6f7065f9e15d588d4cba3
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-03-10 22:09:22 +0700

    first commit

---
+++ screens/Home/index.js
@@ -0,0 +1,15 @@
+import { View, Text} from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+// import MenuBar from '../../component/MenuBar.js/MenuBar';
+
+
+
+export default function Home (){
+    return (
+        <View style={tw`flex-1 items-center justify-center`}>
+            <Text>HomeScsasdreen</Text>
+            {/* <MenuBar /> */}
+        </View>
+    )
+}
---
+++ screens/index.js
@@ -0,0 +1,12 @@
+import Login from './Login';
+import Signup from './Signup';
+import Welcome from './Welcome';
+import Home from './Home';
+
+
+export{
+    Login,
+    Welcome,
+    Home,
+    Signup
+}
